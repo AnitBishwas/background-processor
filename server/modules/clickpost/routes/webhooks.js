106d1666cb167ad2c453b53c4675e564f645e565
AnitBishwas <anit.biswas@swissbeauty.in>
2026-08-26 11:39:14 +0530

    Rto order manual hit enabled

--- server/modules/clickpost/routes/webhooks.js
+++ server/modules/clickpost/routes/webhooks.js
@@ -3,9 +3,9 @@ import { passEventToSqs } from "../controllers/sqs.js";
 
 const clickPostWebhookRoute = Router();
 
-clickPostWebhookRoute.post("/rtoOrder", async (req, res) => {
-  console.log("Click post webhook hit");
-  try {
+clickPostWebhookRoute.post("/rtoOrder/test", async(req,res) =>{
+  try{
+    console.log("test route was hit -->")
     const payload = req.body;
     const shop =
       process.env.NODE_ENV == "dev"
@@ -15,11 +15,29 @@ clickPostWebhookRoute.post("/rtoOrder", async (req, res) => {
     res.status(200).json({
       ok: true,
     });
-  } catch (err) {
+  }catch(err){
     res.status(420).json({
       ok: false,
-    });
+    }); 
   }
-});
+})
+// clickPostWebhookRoute.post("/rtoOrder", async (req, res) => {
+//   console.log("Click post webhook hit");
+//   try {
+//     const payload = req.body;
+//     const shop =
+//       process.env.NODE_ENV == "dev"
+//         ? "swiss-local-dev.myshopify.com"
+//         : "swiss-beauty-dev.myshopify.com";
+//     await passEventToSqs(shop, payload);
+//     res.status(200).json({
+//       ok: true,
+//     });
+//   } catch (err) {
+//     res.status(420).json({
+//       ok: false,
+//     });
+//   }
+// });
 
 export default clickPostWebhookRoute;
